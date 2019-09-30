73790f35ded1322a366664949e4e509fb69ffa24
Raggi <35541698+ragrag@users.noreply.github.com>
2019-09-30 12:37:49 +0200

    added es6 syntax and fixed the reviews not showing

--- layout/scripts/restaurantGallery.js
+++ layout/scripts/restaurantGallery.js
@@ -1,17 +1,21 @@
-var slideIndex = 1;
+const slideIndex = 1;
 showDivs(slideIndex);
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
+const plusDivs = n => {
+  showDivs((slideIndex += n));
+};
 
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("restaurant-gallery");
-    if (n > x.length) {slideIndex = 1} 
-    if (n < 1) {slideIndex = x.length} ;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none"; 
-    }
-    x[slideIndex-1].style.display = "block"; 
-}
+const showDivs = n => {
+  var i;
+  var x = document.getElementsByClassName('restaurant-gallery');
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';
+  }
+  x[slideIndex - 1].style.display = 'block';
+};
--- layout/scripts/showreservationpanel.js
+++ layout/scripts/showreservationpanel.js
@@ -1,13 +1,13 @@
-function showpanel(){
-    
-        document.getElementById("showreservationpanel").addEventListener("click",myFunction);
-}
-        
-function myFunction() {
+const showpaner = () => {
+  document
+    .getElementById('showreservationpanel')
+    .addEventListener('click', myFunction);
+};
 
-    document.getElementById("reservationpanel").style.transition = "all 1s";
-    document.getElementById("reservationpanel").style.paddingTop = "7%";
-    document.getElementById("reservationpanel").style.paddingBottom = "15%";
-    document.getElementById("reservationpanel").style.opacity = 1;
-}
-window.addEventListener("load",showpanel);
+const myFunction = () => {
+  document.getElementById('reservationpanel').style.transition = 'all 1s';
+  document.getElementById('reservationpanel').style.paddingTop = '7%';
+  document.getElementById('reservationpanel').style.paddingBottom = '15%';
+  document.getElementById('reservationpanel').style.opacity = 1;
+};
+window.addEventListener('load', showpanel);
